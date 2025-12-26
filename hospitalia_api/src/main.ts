b57ce670ad246ef77e16d4a229053a5b9ae6b7c3
Bruno Machado <brunovaleriomachado2018@gmail.com>
2025-12-26 18:20:27 -0300

    Api atualizada e funcionando

--- hospitalia_api/src/main.ts
+++ hospitalia_api/src/main.ts
@@ -1,18 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
+;
+import { join } from 'path';
+import * as express from 'express';
+import morgan from 'morgan';
 import { swagConfig } from './utils/swagger_config.ts';
-
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Swagger
+  // Middleware de logs HTTP
+  app.use(morgan('dev'));
+
+  // Middleware CORS - liberar apenas localhost
+  app.enableCors({
+    origin: ['http://localhost:3000'], // pode ajustar se necessário
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
+  });
+  // Configuração do Swagger
   const document = SwaggerModule.createDocument(app, swagConfig);
   SwaggerModule.setup('api', app, document);
-
   const port = 5050;
   await app.listen(port);
   console.log(`🚀 API rodando em http://localhost:${port}/api`);
 }
+
 bootstrap();
